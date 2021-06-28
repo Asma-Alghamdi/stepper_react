52cfa877c101d5eb7630a9e1fd3eda71dd4cfd70
Asma-Alghamdi <assmma143@gmail.com>
2021-06-28 18:07:51 +0300

    First edit App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {render} from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import  Navigation from "./components/Navigation";
+import UploadPage from "./components/UploadPage";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default class App extends Component{
+    constructor(props){
+       super(props); 
+    }
+
+    render(){
+        return( <div className="App">
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route path="/UploadPage" exact component={() => <UploadPage />} />
+          </Switch>
+          <Footer />
+        </Router>
+  
+       
+      </div>);
+    }
 }
 
-export default App;
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
